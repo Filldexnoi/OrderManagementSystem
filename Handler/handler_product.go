be4e2f9810bc3fd6713925541a8edbf3dbf5e20a
Filldexnoi <96350693+Filldexnoi@users.noreply.github.com>
2024-07-17 17:21:29 +0700

    Add log product handler (#14)
    * modifly fiberserver and can show log
    
    * add log handler product

--- Handler/handler_product.go
+++ Handler/handler_product.go
@@ -2,8 +2,10 @@ package Handler
 
 import (
 	"awesomeProject/Usecase"
+	"awesomeProject/observability/logs"
 	"awesomeProject/payload"
 	"github.com/gofiber/fiber/v2"
+	"github.com/sirupsen/logrus"
 )
 
 type ProductHandler struct {
@@ -17,12 +19,15 @@ func NewProductHandler(UseCase Usecase.ProductUseCaseI) ProductHandlerI {
 func (h *ProductHandler) CreateProduct(c *fiber.Ctx) error {
 	product := new(payload.RequestProduct)
 	if err := c.BodyParser(product); err != nil {
+		logs.LogError("Failed to bodyParser product request", logrus.Fields{"error": err.Error()})
 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": err.Error()})
 	}
 	productEntity, err := h.UseCase.CreateProduct(product.ToProduct())
 	if err != nil {
+		logs.LogError("Failed to create product", logrus.Fields{"error": err.Error()})
 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": err.Error()})
 	}
+	logs.LogInfo("Product created successfully", logrus.Fields{"product_id": productEntity.ProductId})
 	ResProduct := payload.ProductToRespondProduct(productEntity)
 	return c.Status(fiber.StatusCreated).JSON(ResProduct)
 }
@@ -30,8 +35,10 @@ func (h *ProductHandler) CreateProduct(c *fiber.Ctx) error {
 func (h *ProductHandler) GetAllProducts(c *fiber.Ctx) error {
 	products, err := h.UseCase.GetAllProducts()
 	if err != nil {
+		logs.LogError("Failed to get all products", logrus.Fields{"error": err.Error()})
 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": err.Error()})
 	}
+	logs.LogInfo("Get all product successfully", nil)
 	var ResProduct []*payload.RespondProduct
 	for _, product := range products {
 		ResProduct = append(ResProduct, payload.ProductToRespondProduct(product))
@@ -42,12 +49,15 @@ func (h *ProductHandler) GetAllProducts(c *fiber.Ctx) error {
 func (h *ProductHandler) GetProductByID(c *fiber.Ctx) error {
 	id, err := c.ParamsInt("id")
 	if err != nil {
+		logs.LogError("Failed to params int product id", logrus.Fields{"error": err.Error()})
 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Invalid product ID"})
 	}
 	product, err := h.UseCase.GetByIDProduct(uint(id))
 	if err != nil {
+		logs.LogError("Failed to get product by id", logrus.Fields{"error": err.Error(), "product_id": id})
 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": err.Error()})
 	}
+	logs.LogInfo("Found product by ID", logrus.Fields{"product_id": product.ProductId})
 	ResProduct := payload.ProductToRespondProduct(product)
 	return c.Status(fiber.StatusOK).JSON(ResProduct)
 }
@@ -55,16 +65,20 @@ func (h *ProductHandler) GetProductByID(c *fiber.Ctx) error {
 func (h *ProductHandler) UpdateProduct(c *fiber.Ctx) error {
 	id, err := c.ParamsInt("id")
 	if err != nil {
+		logs.LogError("Failed to params int product id", logrus.Fields{"error": err.Error()})
 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Invalid product ID"})
 	}
 	product := new(payload.RequestProduct)
 	if err := c.BodyParser(product); err != nil {
+		logs.LogError("Failed to bodyParser product request", logrus.Fields{"error": err.Error()})
 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": err.Error()})
 	}
 	productEntity, err := h.UseCase.UpdateProduct(product.ToProduct(), uint(id))
 	if err != nil {
+		logs.LogError("Failed to update product", logrus.Fields{"error": err.Error(), "product_id": id})
 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": err.Error()})
 	}
+	logs.LogInfo("update product successfully", logrus.Fields{"product_id": productEntity.ProductId})
 	ResProduct := payload.ProductToRespondProduct(productEntity)
 	return c.Status(fiber.StatusOK).JSON(ResProduct)
 }
@@ -72,12 +86,15 @@ func (h *ProductHandler) UpdateProduct(c *fiber.Ctx) error {
 func (h *ProductHandler) DeleteProduct(c *fiber.Ctx) error {
 	id, err := c.ParamsInt("id")
 	if err != nil {
+		logs.LogError("Failed to params int product id", logrus.Fields{"error": err.Error()})
 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Invalid product ID"})
 	}
 	productEntity, err := h.UseCase.DeleteProduct(uint(id))
 	if err != nil {
+		logs.LogError("Failed to delete product", logrus.Fields{"error": err.Error(), "product_id": id})
 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": err.Error()})
 	}
+	logs.LogInfo("Delete product successfully", logrus.Fields{"product_id": productEntity.ProductId})
 	ResProduct := payload.ProductToRespondProduct(productEntity)
 	return c.Status(fiber.StatusOK).JSON(ResProduct)
 }

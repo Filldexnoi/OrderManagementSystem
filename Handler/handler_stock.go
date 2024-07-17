ada24634285f49e8dd1f6cdec6091c78fd58c34a
Filldexnoi <96350693+Filldexnoi@users.noreply.github.com>
2024-07-17 17:21:45 +0700

    Add log stock handler (#16)
    * modifly fiberserver and can show log
    
    * add log in stock handler

--- Handler/handler_stock.go
+++ Handler/handler_stock.go
@@ -2,8 +2,10 @@ package Handler
 
 import (
 	"awesomeProject/Usecase"
+	"awesomeProject/observability/logs"
 	"awesomeProject/payload"
 	"github.com/gofiber/fiber/v2"
+	"github.com/sirupsen/logrus"
 )
 
 type StockHandler struct {
@@ -17,12 +19,15 @@ func NewStockHandler(useCase Usecase.StockUseCaseI) *StockHandler {
 func (h *StockHandler) CreateStock(c *fiber.Ctx) error {
 	stock := new(payload.RequestStock)
 	if err := c.BodyParser(stock); err != nil {
+		logs.LogError("Failed to bodyParser stock request", logrus.Fields{"error": err.Error()})
 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": err.Error()})
 	}
 	stockEntity, err := h.UseCase.CreateStock(stock.ToStock())
 	if err != nil {
+		logs.LogError("Failed to create stock", logrus.Fields{"error": err.Error()})
 		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": err.Error()})
 	}
+	logs.LogInfo("Stock created successfully", logrus.Fields{"product_id": stockEntity.ProductId})
 	ResStock := payload.StockToStockRes(stockEntity)
 	return c.Status(fiber.StatusCreated).JSON(ResStock)
 }
@@ -30,8 +35,10 @@ func (h *StockHandler) CreateStock(c *fiber.Ctx) error {
 func (h *StockHandler) GetAllQtyProducts(c *fiber.Ctx) error {
 	stocks, err := h.UseCase.GetQtyAllProduct()
 	if err != nil {
+		logs.LogError("Failed to get all stocks", logrus.Fields{"error": err.Error()})
 		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": err.Error()})
 	}
+	logs.LogInfo("Get all stocks successfully", nil)
 	var ResStocks []*payload.RespondStock
 	for _, stock := range stocks {
 		ResStocks = append(ResStocks, payload.StockToStockRes(stock))
@@ -42,12 +49,15 @@ func (h *StockHandler) GetAllQtyProducts(c *fiber.Ctx) error {
 func (h *StockHandler) GetQtyProductByID(c *fiber.Ctx) error {
 	id, err := c.ParamsInt("id")
 	if err != nil {
+		logs.LogError("Failed to params int product id", logrus.Fields{"error": err.Error()})
 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Invalid product ID"})
 	}
 	stock, err := h.UseCase.GetQtyByIDProduct(uint(id))
 	if err != nil {
+		logs.LogError("Failed to get stock by product id", logrus.Fields{"error": err.Error(), "product_id": id})
 		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": err.Error()})
 	}
+	logs.LogInfo("Get stock by product ID successfully", logrus.Fields{"product_id": stock.ProductId})
 	ResStock := payload.StockToStockRes(stock)
 	return c.Status(fiber.StatusOK).JSON(ResStock)
 }
@@ -55,16 +65,20 @@ func (h *StockHandler) GetQtyProductByID(c *fiber.Ctx) error {
 func (h *StockHandler) UpdateStock(c *fiber.Ctx) error {
 	id, err := c.ParamsInt("id")
 	if err != nil {
+		logs.LogError("Failed to params int product id", logrus.Fields{"error": err.Error()})
 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Invalid product ID"})
 	}
 	stock := new(payload.RequestStock)
 	if err := c.BodyParser(stock); err != nil {
+		logs.LogError("Failed to bodyParser stock request", logrus.Fields{"error": err.Error()})
 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": err.Error()})
 	}
 	stockEntity, err := h.UseCase.UpdateStock(stock.ToStock(), uint(id))
 	if err != nil {
+		logs.LogError("Failed to update stock", logrus.Fields{"error": err.Error(), "product_id": stock.ProductId})
 		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": err.Error()})
 	}
+	logs.LogInfo("Stock updated successfully", logrus.Fields{"product_id": stockEntity.ProductId})
 	ResStock := payload.StockToStockRes(stockEntity)
 	return c.Status(fiber.StatusOK).JSON(ResStock)
 }
@@ -72,12 +86,15 @@ func (h *StockHandler) UpdateStock(c *fiber.Ctx) error {
 func (h *StockHandler) DeleteStock(c *fiber.Ctx) error {
 	id, err := c.ParamsInt("id")
 	if err != nil {
+		logs.LogError("Failed to params int product id", logrus.Fields{"error": err.Error()})
 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Invalid product ID"})
 	}
 	stockEntity, err := h.UseCase.DeleteStock(uint(id))
 	if err != nil {
+		logs.LogError("Failed to delete stock", logrus.Fields{"error": err.Error(), "product_id": id})
 		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": err.Error()})
 	}
+	logs.LogInfo("Stock deleted successfully", logrus.Fields{"product_id": stockEntity.ProductId})
 	ResStock := payload.StockToStockRes(stockEntity)
 	return c.Status(fiber.StatusOK).JSON(ResStock)
 }

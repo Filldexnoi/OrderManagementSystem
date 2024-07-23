3ef29b2246185358a6f74f9c60c2443926215abb
Filldexnoi <96350693+Filldexnoi@users.noreply.github.com>
2024-07-23 13:34:26 +0700

    Edit mockconform to interface repo

--- Repo/mock_order_repo.go
+++ Repo/mock_order_repo.go
@@ -2,6 +2,7 @@ package Repo
 
 import (
 	"awesomeProject/entities"
+	"context"
 	"github.com/google/uuid"
 	"github.com/stretchr/testify/mock"
 )
@@ -10,32 +11,32 @@ type OrderRepoMock struct {
 	mock.Mock
 }
 
-func (m *OrderRepoMock) SaveCreateOrder(order entities.Order) (*entities.Order, error) {
-	args := m.Called(order)
+func (m *OrderRepoMock) SaveCreateOrder(ctx context.Context, order entities.Order) (*entities.Order, error) {
+	args := m.Called(ctx, order)
 	if args.Get(0) != nil {
 		return args.Get(0).(*entities.Order), args.Error(1)
 	}
 	return nil, args.Error(1)
 }
 
-func (m *OrderRepoMock) GetOrderForUpdateStatus(id uuid.UUID) (*entities.Order, error) {
-	args := m.Called(id)
+func (m *OrderRepoMock) GetOrderForUpdateStatus(ctx context.Context, id uuid.UUID) (*entities.Order, error) {
+	args := m.Called(ctx, id)
 	if args.Get(0) != nil {
 		return args.Get(0).(*entities.Order), args.Error(1)
 	}
 	return nil, args.Error(1)
 }
 
-func (m *OrderRepoMock) SaveUpdateStatusOrder(o entities.Order) (*entities.Order, error) {
-	args := m.Called(o)
+func (m *OrderRepoMock) SaveUpdateStatusOrder(ctx context.Context, o entities.Order) (*entities.Order, error) {
+	args := m.Called(ctx, o)
 	if args.Get(0) != nil {
 		return args.Get(0).(*entities.Order), args.Error(1)
 	}
 	return nil, args.Error(1)
 }
 
-func (m *OrderRepoMock) SaveGetAllOrders() ([]*entities.Order, error) {
-	args := m.Called()
+func (m *OrderRepoMock) SaveGetAllOrders(ctx context.Context) ([]*entities.Order, error) {
+	args := m.Called(ctx)
 	if args.Get(0) != nil {
 		return args.Get(0).([]*entities.Order), args.Error(1)
 	}
--- Repo/mock_transaction_repo.go
+++ Repo/mock_transaction_repo.go
@@ -2,6 +2,7 @@ package Repo
 
 import (
 	"awesomeProject/entities"
+	"context"
 	"github.com/stretchr/testify/mock"
 )
 
@@ -9,24 +10,24 @@ type TransactionRepoMock struct {
 	mock.Mock
 }
 
-func (m *TransactionRepoMock) SaveCreateTransaction(transaction *entities.Transaction) (*entities.Transaction, error) {
-	args := m.Called(transaction)
+func (m *TransactionRepoMock) SaveCreateTransaction(ctx context.Context, transaction *entities.Transaction) (*entities.Transaction, error) {
+	args := m.Called(ctx, transaction)
 	if args.Get(0) != nil {
 		return args.Get(0).(*entities.Transaction), args.Error(1)
 	}
 	return nil, args.Error(1)
 }
 
-func (m *TransactionRepoMock) SaveGetAllTransaction() ([]*entities.Transaction, error) {
-	args := m.Called()
+func (m *TransactionRepoMock) SaveGetAllTransaction(ctx context.Context) ([]*entities.Transaction, error) {
+	args := m.Called(ctx)
 	if args.Get(0) != nil {
 		return args.Get(0).([]*entities.Transaction), args.Error(1)
 	}
 	return nil, args.Error(1)
 }
 
-func (m *TransactionRepoMock) GetTransactionToCreateOrder(order entities.Order) (*entities.Transaction, error) {
-	args := m.Called(order)
+func (m *TransactionRepoMock) GetTransactionToCreateOrder(ctx context.Context, order entities.Order) (*entities.Transaction, error) {
+	args := m.Called(ctx, order)
 	if args.Get(0) != nil {
 		return args.Get(0).(*entities.Transaction), args.Error(1)
 	}

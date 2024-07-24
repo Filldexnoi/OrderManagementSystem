8c4a7b59c4f392f9d43c6de0ffa5ed21f71e4b2e
Filldexnoi <96350693+Filldexnoi@users.noreply.github.com>
2024-07-24 08:03:00 +0700

    Edit mockconform to interface repo (#25)

--- Repo/mock_product_repo.go
+++ Repo/mock_product_repo.go
@@ -2,6 +2,7 @@ package Repo
 
 import (
 	"awesomeProject/entities"
+	"context"
 	"github.com/stretchr/testify/mock"
 )
 
@@ -9,48 +10,48 @@ type ProductRepoMock struct {
 	mock.Mock
 }
 
-func (m *ProductRepoMock) SaveCreateProduct(product entities.Product) (*entities.Product, error) {
-	arguments := m.Called(product)
+func (m *ProductRepoMock) SaveCreateProduct(ctx context.Context, product entities.Product) (*entities.Product, error) {
+	arguments := m.Called(ctx, product)
 	if arguments.Get(0) != nil {
 		return arguments.Get(0).(*entities.Product), arguments.Error(1)
 	}
 	return nil, arguments.Error(1)
 }
 
-func (m *ProductRepoMock) SaveGetAllProduct() ([]*entities.Product, error) {
-	args := m.Called()
+func (m *ProductRepoMock) SaveGetAllProduct(ctx context.Context) ([]*entities.Product, error) {
+	args := m.Called(ctx)
 	if args.Get(0) != nil {
 		return args.Get(0).([]*entities.Product), args.Error(1)
 	}
 	return nil, args.Error(1)
 }
 
-func (m *ProductRepoMock) SaveGetByIDProduct(id uint) (*entities.Product, error) {
-	args := m.Called(id)
+func (m *ProductRepoMock) SaveGetByIDProduct(ctx context.Context, id uint) (*entities.Product, error) {
+	args := m.Called(ctx, id)
 	if args.Get(0) != nil {
 		return args.Get(0).(*entities.Product), args.Error(1)
 	}
 	return nil, args.Error(1)
 }
 
-func (m *ProductRepoMock) SaveUpdateProduct(product entities.Product) (*entities.Product, error) {
-	args := m.Called(product)
+func (m *ProductRepoMock) SaveUpdateProduct(ctx context.Context, product entities.Product) (*entities.Product, error) {
+	args := m.Called(ctx, product)
 	if args.Get(0) != nil {
 		return args.Get(0).(*entities.Product), args.Error(1)
 	}
 	return nil, args.Error(1)
 }
 
-func (m *ProductRepoMock) SaveDeleteProduct(id uint) (*entities.Product, error) {
-	args := m.Called(id)
+func (m *ProductRepoMock) SaveDeleteProduct(ctx context.Context, id uint) (*entities.Product, error) {
+	args := m.Called(ctx, id)
 	if args.Get(0) != nil {
 		return args.Get(0).(*entities.Product), args.Error(1)
 	}
 	return nil, args.Error(1)
 }
 
-func (m *ProductRepoMock) GetPriceProducts(transaction *entities.Transaction) (*entities.Transaction, error) {
-	args := m.Called(transaction)
+func (m *ProductRepoMock) GetPriceProducts(ctx context.Context, transaction *entities.Transaction) (*entities.Transaction, error) {
+	args := m.Called(ctx, transaction)
 	if args.Get(0) != nil {
 		return args.Get(0).(*entities.Transaction), args.Error(1)
 	}
--- Repo/mock_stock_repo.go
+++ Repo/mock_stock_repo.go
@@ -2,6 +2,7 @@ package Repo
 
 import (
 	"awesomeProject/entities"
+	"context"
 	"github.com/stretchr/testify/mock"
 )
 
@@ -9,47 +10,47 @@ type StockRepoMock struct {
 	mock.Mock
 }
 
-func (m *StockRepoMock) SaveCreateStock(stock entities.Stock) (*entities.Stock, error) {
-	args := m.Called(stock)
+func (m *StockRepoMock) SaveCreateStock(ctx context.Context, stock entities.Stock) (*entities.Stock, error) {
+	args := m.Called(ctx, stock)
 	if args.Get(0) != nil {
 		return args.Get(0).(*entities.Stock), nil
 	}
 	return nil, args.Error(1)
 }
 
-func (m *StockRepoMock) SaveGetQtyAllProduct() ([]*entities.Stock, error) {
-	args := m.Called()
+func (m *StockRepoMock) SaveGetQtyAllProduct(ctx context.Context) ([]*entities.Stock, error) {
+	args := m.Called(ctx)
 	if args.Get(0) != nil {
 		return args.Get(0).([]*entities.Stock), nil
 	}
 	return nil, args.Error(1)
 }
 
-func (m *StockRepoMock) SaveGetQtyByIDProduct(id uint) (*entities.Stock, error) {
-	args := m.Called(id)
+func (m *StockRepoMock) SaveGetQtyByIDProduct(ctx context.Context, id uint) (*entities.Stock, error) {
+	args := m.Called(ctx, id)
 	if args.Get(0) != nil {
 		return args.Get(0).(*entities.Stock), nil
 	}
 	return nil, args.Error(1)
 }
 
-func (m *StockRepoMock) SaveUpdateStock(stock entities.Stock) (*entities.Stock, error) {
-	args := m.Called(stock)
+func (m *StockRepoMock) SaveUpdateStock(ctx context.Context, stock entities.Stock) (*entities.Stock, error) {
+	args := m.Called(ctx, stock)
 	if args.Get(0) != nil {
 		return args.Get(0).(*entities.Stock), nil
 	}
 	return nil, args.Error(1)
 }
 
-func (m *StockRepoMock) SaveDeleteStock(id uint) (*entities.Stock, error) {
-	args := m.Called(id)
+func (m *StockRepoMock) SaveDeleteStock(ctx context.Context, id uint) (*entities.Stock, error) {
+	args := m.Called(ctx, id)
 	if args.Get(0) != nil {
 		return args.Get(0).(*entities.Stock), nil
 	}
 	return nil, args.Error(1)
 }
 
-func (m *StockRepoMock) CheckStockToCreateOrder(transaction *entities.Transaction) error {
-	args := m.Called(transaction)
+func (m *StockRepoMock) CheckStockToCreateOrder(ctx context.Context, transaction *entities.Transaction) error {
+	args := m.Called(ctx, transaction)
 	return args.Error(0)
 }
